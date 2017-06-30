781464da51252f63de040265199ff08820865a31
smokfugiu <lucas.salvino@ccc.ufcg.edu.br>
2017-06-30 06:44:57 -0300

    atualizando db

--- include/config.php
+++ include/config.php
@@ -2,10 +2,10 @@
 /**
  * Database configuration
  */
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'gcm_chat');
+define('DB_USERNAME', 'u926774967_visao');
+define('DB_PASSWORD', '123456');
+define('DB_HOST', 'mysql.hostinger.com.br');
+define('DB_NAME', 'u926774967_visao');
 
 define("GOOGLE_API_KEY", "AIzaSyDKk_ew7Vi4FZnMzu6GCY5nLb4xZG8muvI");
 
